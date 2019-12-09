89ba5e1d4acf6214c6086c9a2be3a5f2edbf634f
admin <admin@DESKTOP-IIH6HIE>
2019-12-09 22:02:40 +0530

    demo

---
+++ src/scripts/Test1.java
@@ -0,0 +1,19 @@
+package scripts;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void test3()
+	{
+		System.out.println("1");
+	}
+	public void test1()
+	{
+		System.out.println("2");
+	}
+	public void test2()
+	{
+		System.out.println("3");
+	}
+	}
